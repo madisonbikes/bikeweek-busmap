9f0c710776ed6453b6a8d948876125ccacca78a0
Ben Sandee <tbsandee@orangebikelabs.com>
2022-05-23 09:44:57 -0500

    update for 2022

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,29 @@
 /*
  * Copyright (c) Madison Bikes and Ben Sandee (tbsandee@orangebikelabs.com) 2021.
  */
-
 module.exports = {
-    env: {
-        commonjs: true,
-        es6: true,
-        browser: true,
-        jest: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  plugins: ["@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "react-app",
-        "react-app/jest",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 6,
-    },
-    plugins: ["react", "@typescript-eslint", "no-autofix"],
-    rules: {
-        "prefer-const": "off",
-        "no-autofix/prefer-const": "error"
-    },
+  },
 };
--- src/Constants.ts
+++ src/Constants.ts
@@ -3,32 +3,33 @@
  */
 
 /* Update these BUS IDS to reflect the buses you want to track */
-export const BUS_IDS = getBusIdConfiguration()
+export const BUS_IDS = getBusIdConfiguration();
 
 /* This is should be updated using cron from http://transitdata.cityofmadison.com/Vehicle/VehiclePositions.json */
-export const BUS_LOCATIONS = process.env.REACT_APP_BUS_LOCATION_URI ?? "/MadisonMetroVehiclePositions.json"
+export const BUS_LOCATIONS =
+  process.env.REACT_APP_BUS_LOCATION_URI ?? undefined;
 
 /** update the locations every 10 seconds */
-export const UPDATE_INTERVAL = 10000
+export const UPDATE_INTERVAL = 10000;
 
 /** center of map location coordinates */
-export const MAPS_CENTER = {lat: 43.07472052243664, lng: -89.38414963667884};
+export const MAPS_CENTER = { lat: 43.07472052243664, lng: -89.38414963667884 };
 
 /** initial zoom level */
-export const MAPS_ZOOM_LEVEL = 12
+export const MAPS_ZOOM_LEVEL = 12;
 
 export const MAPS_CONTAINER_STYLE = {
-    width: '800px',
-    height: '600px'
+  width: "800px",
+  height: "600px",
 };
 
-export const MAPS_MAP_ID = "d3aaf7eca4d37317"
+export const MAPS_MAP_ID = "d3aaf7eca4d37317";
 
 function getBusIdConfiguration(): string[] {
-    const configuredBusIds = process.env.REACT_APP_BUS_IDS
-    if(configuredBusIds) {
-        return JSON.parse(configuredBusIds)
-    } else {
-        return ["1904", "012", "129", "2008"]
-    }
+  const configuredBusIds = process.env.REACT_APP_BUS_IDS;
+  if (configuredBusIds) {
+    return JSON.parse(configuredBusIds);
+  } else {
+    return ["1904", "012", "129", "2008"];
+  }
 }

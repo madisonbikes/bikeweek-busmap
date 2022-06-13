3eda03039dfe283c44ac52e95c20594c6788e802
Ben Sandee <tbsandee@orangebikelabs.com>
2022-06-13 14:10:30 -0500

    docs: improve comments about react18 support

--- src/index.tsx
+++ src/index.tsx
@@ -8,5 +8,8 @@ import { App } from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-// FIXME enable <StrictMode></StrictMode> eventually, which doesn't work any more w/React 18. Markers don't show consistently on map.
+// FIXME enable <StrictMode></StrictMode> eventually, which doesn't work any more w/React 18.
+// Markers don't show up consistently because of automatic component mount/unmount designed
+// to ferret out badly-coded apps. I suspect the "badly-coded" app is either @react-google-maps/api
+// or the underlying Google Maps SDK. Will wait for them to fix.
 root.render(<App />);

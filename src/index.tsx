15c3ca360d5c9d3e81fbf344bd674d4688e815fe
Ben Sandee <tbsandee@orangebikelabs.com>
2022-06-19 23:02:15 -0500

    enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@
 import "./index.css";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { StrictMode } from "react";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-// FIXME enable <StrictMode></StrictMode> eventually, which doesn't work any more w/React 18.
-// Markers don't show up consistently because of automatic component mount/unmount designed
-// to ferret out badly-coded apps. I suspect the "badly-coded" app is either @react-google-maps/api
-// or the underlying Google Maps SDK. Will wait for them to fix.
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

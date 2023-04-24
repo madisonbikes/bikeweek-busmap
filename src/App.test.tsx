e5780f37f3ec4e8d56770fcc915a205f88e507f7
Ben Sandee <tbsandee@orangebikelabs.com>
2023-04-24 17:11:24 -0500

    chore: fix test describe/it strings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect } from "vitest";
 import { App } from "./App";
 
-describe("renders Bike Week text", () => {
-  it("thing", async () => {
+describe("App", () => {
+  it("renders bike week text", async () => {
     render(<App />);
     const matched = await screen.findByText(/Bike Week/);
     expect(matched).toBeInTheDocument();
